b5d29d89c7d6d12dd1a45c61d49543ebf50bc67d
Heather White <miss.pomykal@gmail.com>
2022-08-29 12:37:52 -0500

    pe100 - day two - format readability

--- ProjectEuler100/dayTwo/article1.js
+++ ProjectEuler100/dayTwo/article1.js
@@ -1,21 +1,27 @@
 function fiboEvenSum(n) {
+
     //in the case our input "n" is less than 2, let's return 0
     if(n <= 1) {
       return 0;
     }
+
     //create variables for our sum and our pointers
     let sum = 0, prev = 0, curr = 1, next;
+
     //only evaluate numbers that do not exceed input "n"
     while(curr <= n) {
+
     //check if our current number in the fib sequence is even
     //using the modulo operator to evaluate the remainder    
       if(curr % 2 === 0) {
         sum += curr;
       }
+
     //iterate by swapping variables:
       next = curr + prev;
       prev = curr;
       curr = next;
     }
+    
     return sum;
   }
