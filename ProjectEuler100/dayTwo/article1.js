84937b94a15c3c88c83842ff2efae0c7e8333aac
Heather White <miss.pomykal@gmail.com>
2022-08-30 18:08:23 -0500

    edit: more readable comments

--- ProjectEuler100/dayTwo/article1.js
+++ ProjectEuler100/dayTwo/article1.js
@@ -6,22 +6,29 @@ function fiboEvenSum(n) {
     }
 
     //create variables for our sum and our pointers
+    //start our fib sequence as follows: 0(prev), 1(curr) 
     let sum = 0, prev = 0, curr = 1, next;
 
     //only evaluate numbers that do not exceed input "n"
     while(curr <= n) {
 
-    //check if our current number in the fib sequence is even
-    //using the modulo operator to evaluate the remainder    
+    //we only want to add even numbers to our sum
+    //so let's check if our current number in the fib sequence
+    //is even using the modulo operator to evaluate the remainder    
       if(curr % 2 === 0) {
         sum += curr;
       }
 
-    //iterate by swapping variables:
+    //now we iterate by swapping variables:
+    //the next fib num is the result of adding 
+    //the previous two nums in the fib sequence so far
       next = curr + prev;
+
+    //nudge our pointers "forward" a variable
+    //and head back to our while loop condition
       prev = curr;
       curr = next;
     }
-    
+
     return sum;
   }
